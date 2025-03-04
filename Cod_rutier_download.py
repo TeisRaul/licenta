57b05a3a4c40bbab49a2071b447f9c295fb94595
TeisRaul <147483530+TeisRaul@users.noreply.github.com>
2025-03-05 01:18:26 +0200

    write them with html code, but no diacritics

--- Cod_rutier_download.py
+++ Cod_rutier_download.py
@@ -6,7 +6,9 @@ response = requests.get(file_url)
 
 if response.status_code == 200:
     lines = response.text.splitlines()
-    for line in lines:
-        print(line)
+    with open("cod_rutier.txt", "w", encoding="utf-8") as file:
+        for line in lines:
+            file.write(line)
+    print("File downloaded successfully")
 else:
     print("Failed to download file. Status code: ", response.status_code)
