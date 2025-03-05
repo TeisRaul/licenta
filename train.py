90071dd9dd690d7bb23dc2e94bfd23c2b05d83af
TeisRaul <147483530+TeisRaul@users.noreply.github.com>
2025-03-05 02:05:49 +0200

    add  words

--- train.py
+++ train.py
@@ -3,8 +3,8 @@ import random
 import torch
 import torch.nn as nn
 from torch.utils.data import Dataset, DataLoader
-#from functions import bag_of_words, tokenize, stem
-#from model import NeuralNet
+from functions import bag_of_words, tokenize, stem
+from model import NeuralNet
 
 filepaths = [
     r"C:\Users\teisr\.vscode\chatbot-romanian-laws\sources\cod_rutier.txt",
@@ -16,5 +16,20 @@ all_content = []
 for filepath in filepaths:
     with open(filepath, "r", encoding="utf-8") as file:
         all_content.append(file.read())
+
+all_words = []
+tags = []
+xy = []
+
+for content in all_content:
+    tag = all_content.index(content)
+    tags.append(tag)
+    for line in content.split("\n"):
+        tokenized = tokenize(line)
+        all_words.extend(tokenized)
+        xy.append((tokenized, tag))
         
-print(all_content)
+ignore_words = ["?", "!", ".", ","]
+all_words = [stem(word) for word in all_words if word not in ignore_words]
+all_words = sorted(set(all_words))
+tags = sorted(set(tags))
