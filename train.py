764e15ba90190db6e72b97805fc4e47aec38e1ed
TeisRaul <147483530+TeisRaul@users.noreply.github.com>
2025-03-05 02:15:40 +0200

    chatdataset(dataset)

--- train.py
+++ train.py
@@ -56,3 +56,17 @@ input_size = len(X_train[0])
 hidden_size = 32
 output_size = len(tags)
 print(input_size, output_size)
+
+class ChatDataset(Dataset):
+    def __init__(self):
+        self.n_samples = len(X_train)
+        self.x_data = X_train
+        self.y_data = y_train
+        
+    def __getitem__(self, index):
+        return self.x_data[index], self.y_data[index]
+
+    def __len__(self):
+        return self.n_samples
+
+dataset = ChatDataset()
